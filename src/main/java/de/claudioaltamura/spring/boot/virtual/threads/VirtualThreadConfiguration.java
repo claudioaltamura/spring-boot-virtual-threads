8bda0e3d8669ab7f69f4a30742e6a7ece992454b
Claudio Altamura <claudio@claudioaltamura.de>
2023-10-24 16:59:14 +0200

    feat(config): Embracing Virtual Threads.

---
+++ src/main/java/de/claudioaltamura/spring/boot/virtual/threads/VirtualThreadConfiguration.java
@@ -0,0 +1,28 @@
+package de.claudioaltamura.spring.boot.virtual.threads;
+
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+
+import java.util.concurrent.Executors;
+
+/**
+ * Embracing Virtual Threads.
+ * @see <a href="https://spring.io/blog/2022/10/11/embracing-virtual-threads"/>
+ */
+@Configuration
+public class VirtualThreadConfiguration {
+
+    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+    public AsyncTaskExecutor asyncTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    }
+}
